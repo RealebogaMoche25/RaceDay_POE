6d444f1104b69a44949020663d770829822ae565
RealebogaMoche25 <st10486074@rcconnect.edu.za>
2026-08-31 19:54:51 +0200

    Create USER table for system accounts and roles

--- RACEDAY_POE.sql
+++ RACEDAY_POE.sql
@@ -31,3 +31,38 @@
 
 CREATE DATABASE RACEDAY_POE;
 USE RACEDAY_POE;
+
+/* ============================================================
+   1. USER TABLE
+   ============================================================
+   Stores information about all users of the RaceDay system.
+   Organisers and Participants are NOT stored in separate
+   tables. Instead, both are stored as users and their role
+   is identified using the ROLE column.
+   ROLE can only contain:
+   - Organiser
+   - Participant
+   */
+
+CREATE TABLE [USER]
+(
+    USER_ID INT IDENTITY(1,1) PRIMARY KEY,
+    FIRST_NAME VARCHAR(50) NOT NULL,
+    LAST_NAME VARCHAR(50) NOT NULL,
+    -- UNIQUE prevents two users from registering with the same email address.
+    EMAIL VARCHAR(100) NOT NULL UNIQUE,
+    PHONE_NUMBER VARCHAR(20),
+    -- Stores the hashed version of the user's password. The original/plain-text password must NOT be stored.
+    PASSWORD_HASH VARCHAR(255) NOT NULL,
+    -- Determines whether the user is an Organiser or a Participant.
+    ROLE VARCHAR(20) NOT NULL,
+    --NULL Because when someone first registers, they might not have uploaded a profile picture yet. A user can exist even if they don't have a profile picture yet.
+    PROFILE_PICTURE_URL VARCHAR(500) NULL,
+
+    -- Ensures that only the two approved roles can be stored in the ROLE column.
+    CONSTRAINT CK_USER_ROLE
+        CHECK (ROLE IN ('Organiser', 'Participant'))
+);
+
+SELECT * FROM [USER];
+
