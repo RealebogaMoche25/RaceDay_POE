9c2b7a9cee148b80ba7c26bb64734c5b786e6460
RealebogaMoche25 <st10486074@rcconnect.edu.za>
2026-09-16 02:50:35 +0200

    add database context and RaceDay models

---
+++ PART2/RACEDAY_API/Data/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace RACEDAY_API.Data
+{
+    public class ApplicationDBContext : DbContext
+{
+	public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+	{
+	}
+}
---
+++ PART2/RACEDAY_API/Models/CATEGORY.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace RACEDAY_API.Models
+{
+    public class CATEGORY
+    {
+
+    }
+}
