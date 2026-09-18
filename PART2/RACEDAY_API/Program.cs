1a70ae8c8955686b97215a92148a2b5eb42e4129
RealebogaMoche25 <st10486074@rcconnect.edu.za>
2026-09-18 09:53:09 +0200

    added the DbContext registration

--- PART2/RACEDAY_API/Program.cs
+++ PART2/RACEDAY_API/Program.cs
@@ -1,3 +1,7 @@
+using API_proj.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"))
+    }
+);      
 
 var app = builder.Build();
 
