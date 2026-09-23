baff605f7909212089c8c4e76695b06fe483d047
RealebogaMoche25 <st10486074@rcconnect.edu.za>
2026-09-23 21:31:06 +0200

    add registration request DTO

---
+++ PART2/RACEDAY_API/Models/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿namespace RACEDAY_API.Models
+{
+    public class RegisterDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
